50c8e30260fc24f96da7eb52953d9e551df7a940
Amy Ma <echo@Amys-Laptop.local>
2026-05-12 17:23:12 +0800

    Deploy Amy digital resume

---
+++ _next/static/9xqGxHGaawX1bqOHcsurl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/867-6c13947b9141af59.js","static/chunks/pages/index-ef6d1152406f4cc3.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
