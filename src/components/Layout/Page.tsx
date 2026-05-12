196ffd0a85e1fe3f2e1df727a8a7b70ca3c375ac
Amy Ma <echo@192.168.5.36>
2026-05-12 21:54:27 +0800

    Fix GitHub Pages styling and add resume assets

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -5,8 +5,11 @@ import {memo, PropsWithChildren} from 'react';
 
 import {HomepageMeta} from '../../data/dataDef';
 
+const siteUrl = 'https://amymacumc.github.io/amy-digital-resume';
+
 const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, description}) => {
   const {asPath: pathname} = useRouter();
+  const canonicalPath = pathname === '/' ? '' : pathname;
 
   return (
     <>
@@ -14,18 +17,15 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         <title>{title}</title>
         <meta content={description} name="description" />
 
-        {/* several domains list the same content, make sure google knows we mean this one. */}
-        <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical" />
+        <link href={`${siteUrl}${canonicalPath}`} key="canonical" rel="canonical" />
 
-        <link href="/favicon.ico" rel="icon" sizes="any" />
-        <link href="/icon.svg" rel="icon" type="image/svg+xml" />
-        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
-        <link href="/site.webmanifest" rel="manifest" />
+        <link href="./favicon.ico" rel="icon" sizes="any" />
+        <link href="./site.webmanifest" rel="manifest" />
 
         {/* Open Graph : https://ogp.me/ */}
         <meta content={title} property="og:title" />
         <meta content={description} property="og:description" />
-        <meta content={`https://reactresume.com${pathname}`} property="og:url" />
+        <meta content={`${siteUrl}${canonicalPath}`} property="og:url" />
 
         {/* Twitter: https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */}
         <meta content={title} name="twitter:title" />
