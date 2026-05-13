7b4366118f4c056a0b936f5e3e3549397dc35091
Amy Ma <echo@Amys-Laptop.local>
2026-05-13 15:00:54 +0800

    Deploy hero portrait update

---
+++ _next/static/sQ5tjtxQMudXkw7G3j0da/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/867-6c13947b9141af59.js","static/chunks/pages/index-280e83c60d0f914c.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
